d89cfb8f19601c2b2d4444a694d5cd7b166581ac
Oshada1988 <lahirupr471@gmail.com>
2021-08-07 10:15:00 +0530

    implement fetch all posts api

--- Issue-Tracking-Systems/routes/api.php
+++ Issue-Tracking-Systems/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+//import posts
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//create this route for fetch all posts
+Route::get('/posts',function (Request $request){
+    return Post :: all();
+});
+
